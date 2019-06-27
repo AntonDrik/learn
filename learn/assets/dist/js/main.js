8f9d07f188627e31067969b8aae58661846cfc96
AntonDrik <bragimbragim@gmail.com>
2019-06-27 17:44:06 +0300

    add multiplay

--- learn/assets/dist/js/main.js
+++ learn/assets/dist/js/main.js
@@ -8,22 +8,76 @@ var CONST_TEXT_EQUAL = "Ничья";
 var CONST_TEXT_WIN = "Победил: ";
 var CONST_TEXT_CHOOSE = "Выбрано: ";
 var gameObj = { // игровой объект
-	players: [ // массив игроков
-		player1 = {name: "Игрок 1"},
-		player2 = {name: "Игрок 2"},
-	],
+
+	compareData: {
+		rock: false,
+		scissors: false,
+		paper: false
+	},
+
+	players: [],
+
+	setPlayers: function(playersCount){
+
+		let playersWrapper = $('.players');
+		let playersBlock = $('.player');
+
+		for (let i = 0; i < playersCount; i++){
+			let name = prompt("Введите имя "+(i+1)+" игрока");
+
+			let tmpPlayersBlock = playersBlock.clone(true).addClass("active_player");
+			tmpPlayersBlock.find('.btn__player').text(name);
+			tmpPlayersBlock.find('.player__result').addClass("player__result-active");
+			playersWrapper.append(tmpPlayersBlock);
+
+			this.players.push({
+				name: name,
+				HTMLBlock: tmpPlayersBlock
+			});
+		}
+
+		$('.active_player').css("visibility", "visible");
+
+	},
 	compare: function () { //сравнение элементов
-		let concatResult = +(this.players[0].choose / this.players[1].choose).toFixed(1); //делю элементы чтобы получить уникальное значение
-		if (this.players[0].choose === this.players[1].choose) // если элементы равны - ничья
-			return CONST_TEXT_EQUAL;
-		else if (concatResult === 0.5 || concatResult === 0.7 || concatResult === 3.0) //проверка уникальных значений для первого игрока, если равно - первый игрок победил
-			return CONST_TEXT_WIN + this.players[0].name;
-		else // иначе - победил второй игрок
-			return CONST_TEXT_WIN + this.players[1].name;
+
+		if (this.compareData.rock &&
+			this.compareData.paper &&
+			this.compareData.scissors){
+			alert(CONST_TEXT_EQUAL);
+		} else{
+			let tmpPlayers = Object.assign([], this.players);
+			tmpPlayers.sort(sortMas);
+			let concatResult = +(tmpPlayers[0].choose / tmpPlayers[tmpPlayers.length-1].choose).toFixed(1); //делю элементы чтобы получить уникальное значение
+			if (tmpPlayers[0].choose === tmpPlayers[tmpPlayers.length-1].choose){
+				alert(CONST_TEXT_EQUAL);
+			}
+			else if (concatResult === 0.5 || concatResult === 0.7 || concatResult === 3.0){
+				for(let i = 0; i < tmpPlayers.length; i++){
+					if (tmpPlayers[i].choose!==tmpPlayers[i+1].choose) {
+						tmpPlayers[i].HTMLBlock.css("background-color", "green");
+						break;
+					}
+					else{
+						tmpPlayers[i].HTMLBlock.css("background-color", "green");
+					}
+				}
+			} else{
+				for(let i = tmpPlayers.length-1; i >=0; i--){
+					if (tmpPlayers[i].choose!==tmpPlayers[i-1].choose) {
+						tmpPlayers[i].HTMLBlock.css("background-color", "green");
+						break;
+					}
+					else{
+						tmpPlayers[i].HTMLBlock.css("background-color", "green");
+					}
+				}
+			}
+		}
 	},
 	removeChoose: function () {
 		let i = 0;
-		for (let item of $('.players__result')){ // вывожу на экран имена выбранных элементов
+		for (let item of $('.player__result-active')){ // вывожу на экран имена выбранных элементов
 			item.innerHTML = CONST_TEXT_CHOOSE+gameObj.players[i].chooseName;
 			delete gameObj.players[i].choose;
 			delete gameObj.players[i].chooseName;
@@ -32,18 +86,31 @@ var gameObj = { // игровой объект
 	}
 };
 
+function sortMas( a, b ) {
+	if ( a.choose > b.choose ){
+		return -1;
+	}
+	if ( a.choose < b.choose ){
+		return 1;
+	}
+	return 0;
+}
+
 $(document).ready(function() {
 
-	var playerEl = $('.btn__player'); // массив кнопок
+
+	$('.btn__start').on ("click", function(){
+		let playerCount = prompt("Enter number of players", "");
+		 if (isFinite(+playerCount) && +playerCount>1 && +playerCount<10) {
+		 	gameObj.setPlayers(+playerCount);
+		 } else {
+		 	alert('value is not valid');
+		 }
+	});
+
 	var clickedEl;
 
-	let i = 0;
-	for (let item of playerEl){ //прохожу по кнопкам и присваиваю каждой кнопке свое имя из массива игроков
-		item.innerHTML = gameObj.players[i].name;
-		i++;
-	}
-
-	playerEl.magnificPopup({
+	$('.btn__player').magnificPopup({
 		type: 'inline',
 
 		fixedContentPos: false,
@@ -60,8 +127,8 @@ $(document).ready(function() {
 	});
 
 
-	playerEl.on("click", function () {
-		clickedEl = playerEl.index(this); // запоминаю игрока который делает выбор
+	$('.btn__player').on("click", function () {
+		clickedEl = $('.btn__player').index(this)-1; // запоминаю игрока который делает выбор
 		$('#playerName').text(gameObj.players[clickedEl].name); // вывожу имя игрока во всплывающее окно
 	});
 
@@ -69,8 +136,21 @@ $(document).ready(function() {
 			gameObj.players[clickedEl].choose = ($('.items__card').index(this))+1; // добавляю в объект игрока index выбранного элемента (камень - 1, ножницы - 2, бумага - 3)
 			gameObj.players[clickedEl].chooseName = this.textContent; // туда же добавляю название выбранного элемента
 
-			if (gameObj.players[0].choose !== undefined && gameObj.players[1].choose !== undefined){ // если у обоих игроков выбран элемент вызываю функцию сравнения
+			if (gameObj.players[clickedEl].choose===1){
+				gameObj.compareData.rock = true;
+			}
 
+			else if (gameObj.players[clickedEl].choose===2){
+				gameObj.compareData.scissors = true;
+			}
+
+			else if (gameObj.players[clickedEl].choose===3){
+				gameObj.compareData.paper = true;
+			}
+
+
+			let tmpGameObj = gameObj.players.filter(item => item.choose === undefined);
+			if (!tmpGameObj.length){ // если у обоих игроков выбран элемент вызываю функцию сравнения
 				$('.players__winner').text(gameObj.compare());
 				gameObj.removeChoose();
 			}
