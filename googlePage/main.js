4f569bd709850f1e618fc056e84472ff250fc059
AntonDrik <bragimbragim@gmail.com>
2019-07-11 18:02:32 +0300

    upd

---
+++ googlePage/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.header').addEventListener('click', (e)=>{
+    alert('1');
+});
+
+document.querySelector('.header__link>a').addEventListener('click', (e)=>{
+    alert('2');
+});
+document.querySelector('.header__link>a').addEventListener('click', (e)=>{
+    alert('4');
+});
+document.querySelector('.header__link>a').addEventListener('click', (e)=>{
+    alert('5');
+});
